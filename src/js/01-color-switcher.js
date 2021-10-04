04aa4d6688052192ef7b9426a73a37229ecfe04f
sandra9ermak <sandra1ermak@gmail.com>
2021-10-04 15:45:58 +0300

    start

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,17 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timer = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => {
+    timer = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+});
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(timer);
+});
