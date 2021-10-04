476d1f99e2a7084a9d317f0974aed18ccb6c8f15
sandra9ermak <88450606+sandra9ermak@users.noreply.github.com>
2021-10-04 21:57:36 +0000

    Deploying to gh-pages from @ sandra9ermak/goit-js-hw-09@108c92449d1f9f4620d5c3db1f3bc16b97899b44 🚀

--- 02-timer.85bdce8c.js
+++ 02-timer.85bdce8c.js
@@ -17,6 +17,6 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./types/options":"lJVm","./l10n/default":"SFEe","./utils":"U4Vg","./utils/dom":"UwHZ","./utils/dates":"viQd","./utils/formatting":"KtbI","./utils/polyfills":"pFfd"}],"c0Wa":[function(require,module,exports) {
 
 },{}],"d2uN":[function(require,module,exports) {
-"use strict";var r=e(require("flatpickr"));function e(r){return r&&r.__esModule?r:{default:r}}require("flatpickr/dist/flatpickr.min.css");
+"use strict";var e=t(require("flatpickr"));function t(e){return e&&e.__esModule?e:{default:e}}function r(e){return{days:Math.floor(e/864e5),hours:Math.floor(e%864e5/36e5),minutes:Math.floor(e%864e5%36e5/6e4),seconds:Math.floor(e%864e5%36e5%6e4/1e3)}}require("flatpickr/dist/flatpickr.min.css");const o={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0])}},n=document.querySelector("input#datetime-picker"),a=document.querySelector("[data-start]"),l=new Date,i=new Date(n.value);(0,e.default)(n,o),a.addEventListener("click",e=>{l.getTime();console.log()});
 },{"flatpickr":"BKNP","flatpickr/dist/flatpickr.min.css":"c0Wa"}]},{},["d2uN"], null)
-//# sourceMappingURL=/goit-js-hw-09/02-timer.c7fe2682.js.map
+//# sourceMappingURL=/goit-js-hw-09/02-timer.85bdce8c.js.map
