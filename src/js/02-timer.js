108c92449d1f9f4620d5c3db1f3bc16b97899b44
sandra9ermak <sandra1ermak@gmail.com>
2021-10-05 00:56:49 +0300

    2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,58 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+const dateSelector = document.querySelector('input#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+const dateNow = new Date();
+const dateInput = new Date(dateSelector.value);
+flatpickr(dateSelector, options);
+
+//const getNowDate = dateNow.getDay + ' '
+
+startBtn.addEventListener('click', event => {
+    // if (dateSelector.value - dateNow.value === 0) {
+    //     window.alert('ok');
+    //     //startBtn.removeAttribute('disabled');
+    // }
+    // if (dateSelector.value < dateNow.getDay()) {
+    //     console.log('oj');
+    //     window.alert("Please choose a date in the future");
+    //     //startBtn.setAttribute('disabled', 'disabled');
+    // }
+
+    //console.log(convertMs(dateNow) - dateSelector.value);
+    const getNowTime = dateNow.getTime();
+    //const getInputTime = dateSelector.getTime();
+    //console.log(dateSelector.value);
+    //console.log(dateSelector.value.getTime());
+    console.log();
+});
